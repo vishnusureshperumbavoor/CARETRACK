7c847f309d03c6f19cddeb548c09d327be22a16c
Akshay M M <113523561+akshaymmaimbilly@users.noreply.github.com>
2024-02-06 15:33:18 +0530

    update

--- frontent/src/components/Dashboard.jsx
+++ frontent/src/components/Dashboard.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
-
+import Grid from '@mui/material/Grid';
+import MenuItem from '@mui/material/MenuItem';
 
 const Dashboard = () => {
   const [patientData, setPatientData] = useState({
@@ -98,10 +99,8 @@ const Dashboard = () => {
   };
 
   return (
-    
-     
-    <div   className="formContainer fade-in">
-      <div  style={{
+    <div className="formContainer fade-in">
+      <div style={{
         maxWidth: '1000px',
         margin: '200px',
         padding: '20px',
@@ -111,137 +110,100 @@ const Dashboard = () => {
       }}>
         <h1 style={{ textAlign: 'center', marginBottom: '30px' }}>MEDICAL REPORT</h1>
         <form onSubmit={handleSubmit}>
-          {/* Personal Details */}
-          <h1>Personal details</h1>
-         
-          <div style={{ marginRight: '40px' }}>
-            <label>Id No:</label>
+          <Grid container spacing={3}>
+            {/* Personal Details */}
+            <Grid item xs={12}>
+              <h2>Personal details</h2>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField fullWidth label="Id No" name="personalDetails.id" required value={patientData.personalDetails.id} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField fullWidth label="Name" name="personalDetails.name" required value={patientData.personalDetails.name} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Gender" name="gender" required select value={patientData.personalDetails.gender} onChange={handleChange}>
+                <MenuItem value="Male">Male</MenuItem>
+                <MenuItem value="Female">Female</MenuItem>
+                <MenuItem value="Other">Other</MenuItem>
+              </TextField>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField fullWidth label="Date of Birth" name="dob" type="date" required value={patientData.personalDetails.dob} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField fullWidth label="Age" name="age" value={patientData.personalDetails.age} readOnly />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Place" name="personalDetails.place" required value={patientData.personalDetails.place} onChange={handleChange} />
+            </Grid>
 
-           <input type="text" name="personalDetails.id"  required value={patientData.personalDetails.id} onChange={handleChange}  /> 
-           
-           
-           
-           
-          <label>Name:</label>
-            <input type="text"  name="personalDetails.name" required value={patientData.personalDetails.name} onChange={handleChange} />
+            {/* Contact Details */}
+            <Grid item xs={12}>
+              <h2>Contact Details</h2>
+            </Grid>
+            <Grid item xs={4}>
+              <TextField fullWidth label="Phone Number" name="contactDetails.phoneNumber" required value={patientData.contactDetails.phoneNumber} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField fullWidth label="Bystander Phone" name="contactDetails.bystanderPhoneNumber" required value={patientData.contactDetails.bystanderPhoneNumber} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField fullWidth label="Email Id" name="contactDetails.email" required value={patientData.contactDetails.email} onChange={handleChange} />
+            </Grid>
 
-           {/* Add spacing here */}
-           <div style={{ marginRight: '400px' }}>
-              <label>Gender:</label>
-              <div>
-                <label>
-                  
-                  
-                  <input required
-                    type="radio"
-                    name="gender"
-                    value="Male"
-                    checked={patientData.personalDetails.gender === 'Male'}
-                    onChange={handleChange}
-                  />
-                  Male
-                </label>
-                <label>
-                  <input required
-                    type="radio"
-                    name="gender"
-                    value="Female"
-                    checked={patientData.personalDetails.gender === 'Female'}
-                    onChange={handleChange}
-                  />
-                  Female
-                </label>
-                <label>
-                  <input required
-                    type="radio"
-                    name="gender"
-                    value="Other"
-                    checked={patientData.personalDetails.gender === 'Other'}
-                    onChange={handleChange}
-                  />
-                  Other
-                </label>
+            {/* Medical Records */}
+            <Grid item xs={12}>
+              <h2>Medical Records</h2>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Allergy" name="medicalRecords.allergies" required value={patientData.medicalRecords.allergies} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Medical Alerts" name="medicalRecords.medicalAlerts" required value={patientData.medicalRecords.medicalAlerts} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Principle Doctor" name="medicalRecords.principleDoctor" required value={patientData.medicalRecords.principleDoctor} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Hospital" name="medicalRecords.hospital" required value={patientData.medicalRecords.hospital} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Reason For Admission" name="medicalRecords.reasonForAdmission" required value={patientData.medicalRecords.reasonForAdmission} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Principle Diagnosis" name="medicalRecords.principleDiagnosis" required value={patientData.medicalRecords.principleDiagnosis} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Other Diagnosis" name="medicalRecords.otherDiagnosis" required value={patientData.medicalRecords.otherDiagnosis} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Operation Procedure" name="medicalRecords.operationProcedure" required value={patientData.medicalRecords.operationProcedure} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Description" name="medicalRecords.description" required value={patientData.medicalRecords.description} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Medicines" name="medicalRecords.medicines" required value={patientData.medicalRecords.medicines} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Patient History" name="medicalRecords.patientHistory" required value={patientData.medicalRecords.patientHistory} onChange={handleChange} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth label="Treatment" name="medicalRecords.treatments" required value={patientData.medicalRecords.treatments} onChange={handleChange} />
+            </Grid>
+
+            {/* Buttons */}
+            <Grid item xs={12}>
+              <div style={{ marginBottom: '50px' }}>
+                <button type="submit">Save</button>
+                <button type="reset">Reset</button>
               </div>
-            </div>
-            {/* Add spacing here */}
-          
-            <label>Date of Birth:</label>
-            <input style={{ marginBottom: '15px' }} type="date" name="dob" required value={patientData.personalDetails.dob} onChange={handleChange} />
-      
-
-            <label>Age:</label>
-            <input style={{ marginBottom: '15px' }} type="text" required name="age" value={patientData.personalDetails.age} readOnly />
-
-            <label>Place:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="personalDetails.place" required value={patientData.personalDetails.place} onChange={handleChange} />
-
-            {/* Add other personal details fields here */}
-          </div>
-
-          {/* Contact Details */}
-          <h1>Contact Details</h1>
-          <div style={{ marginBottom: '40px' }}>
-            <label>Phone Number:</label>
-            <input type="number" name="contactDetails.phoneNumber" required value={patientData.contactDetails.phoneNumber} onChange={handleChange} />
-
-            <label>Bystander Phone:</label>
-            <input style={{ marginBottom: '15px' }} type="number" name="contactDetails.bystanderPhoneNumber" required value={patientData.contactDetails.bystanderPhoneNumber} onChange={handleChange} />
-
-            <label>Email Id:</label>
-            <input style={{ marginBottom: '15px' }} type="email" name="contactDetails.email" required value={patientData.contactDetails.email} onChange={handleChange} />
-            {/* Add other contact details fields here */}
-          </div>
-
-          {/* Medical Records */}
-          <h1>Medical Records</h1>
-          <div style={{ marginBottom: '40px' }}>
-            <label>Allergy:</label>
-            <input type="text" name="medicalRecords.allergies" required value={patientData.medicalRecords.allergies} onChange={handleChange} />
-
-            <label>Medical Alerts:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.medicalAlerts" required value={patientData.medicalRecords.medicalAlerts} onChange={handleChange} />
-
-            <label>Principle Doctor:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.principleDoctor" required value={patientData.medicalRecords.principleDoctor} onChange={handleChange} />
-
-            <label>Hospital:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.hospital" required value={patientData.medicalRecords.hospital} onChange={handleChange} />
-
-            <label>Reason For Admission:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.reasonForAdmission" required value={patientData.medicalRecords.reasonForAdmission} onChange={handleChange} />
-
-            <label>Principle Diagnosis:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.principleDiagnosis" required value={patientData.medicalRecords.principleDiagnosis} onChange={handleChange} />
-
-            <label>Other Diagnosis:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.otherDiagnosis" required value={patientData.medicalRecords.otherDiagnosis} onChange={handleChange} />
-
-            <label>Operation Procedure:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.operationProcedure" required value={patientData.medicalRecords.operationProcedure} onChange={handleChange} />
-
-            <label>Description:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.description" required value={patientData.medicalRecords.description} onChange={handleChange} />
-
-            <label>Medicines:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.medicines" required value={patientData.medicalRecords.medicines} onChange={handleChange} />
-
-            <label>Patient History:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.patientHistory" required value={patientData.medicalRecords.patientHistory} onChange={handleChange} />
-
-            <label>Treatment:</label>
-            <input style={{ marginBottom: '15px' }} type="text" name="medicalRecords.treatments" required value={patientData.medicalRecords.treatments} onChange={handleChange} />
-
-            {/* Add other medical records fields here */}
-          </div>
-
-          <div style={{ marginBottom: '50px' }}>
-            <button type="submit">Save</button>
-            <button type="reset">Reset</button>
-          </div>
+            </Grid>
+          </Grid>
         </form>
       </div>
     </div>
-   
   );
 };
 
